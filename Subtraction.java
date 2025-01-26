b5d69756cebaa11e6d24389acca5d659a1769429
rupamkumarbarnwal <rupamkumar899@gmail.com>
2025-01-26 16:00:32 +0530

    Initial files added

---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+public class Subtraction{
+   public static void main(String[]args){
+        int a = 78;
+        int b = 67;
+        int result = a-b;
+        System.out.println(result);
+}
+
+}
